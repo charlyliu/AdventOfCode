ab053e06380495362f6ac89c73723abe8498910d
Charly Liu Chou <charly.liu@adevinta.com>
2024-12-02 15:12:14 +0100

    day 1 part 2 2024

--- 2024/1.lua
+++ 2024/1.lua
@@ -12,25 +12,36 @@ end
 function ConstructSortedTables(input)
 	local left = {}
 	local right = {}
+	local rightOccurances = {}
 
 	for line in io.lines(input) do
 		local lineSplitted = Split(line, " ")
 
 		table.insert(left, tonumber(lineSplitted[1]))
-		table.insert(right, tonumber(lineSplitted[4]))
+		local rightNumber = tonumber(lineSplitted[4])
+		local rightString = lineSplitted[4]
+		table.insert(right, rightNumber)
+		rightOccurances[rightString] = (rightOccurances[rightString] or 0) + 1
 	end
 
 	table.sort(left)
 	table.sort(right)
 
-	return left, right
+	return left, right, rightOccurances
 end
 
-local listLeft, listRight = ConstructSortedTables("input_1.txt")
+local listLeft, listRight, tableOccurances = ConstructSortedTables("input_1.txt")
 
 local distance = 0
+local similarities = 0
+
 for index, value in ipairs(listLeft) do
 	distance = distance + math.abs(value - listRight[index])
+	similarities = similarities + (value * (tableOccurances[tostring(value)] or 0))
 end
 
-print(distance)
+-- Part 1
+print("distance: ", tostring(distance))
+
+-- Part 2
+print("similarities: ", tostring(similarities))
